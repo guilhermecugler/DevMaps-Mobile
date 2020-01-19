26ab81d03755c67085728bea4162776dc296eb2e
Guilherme Cugler <guilhermecugler@gmail.com>
2020-01-19 17:58:51 -0300

    Envio completo

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Routes from "./src/routes";
+import { StatusBar, YellowBox } from "react-native";
+
+YellowBox.ignoreWarnings(["Unrecognized WebSocket"]);
+
+export default function App() {
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#c0392b" />
+      <Routes />
+    </>
+  );
+}
